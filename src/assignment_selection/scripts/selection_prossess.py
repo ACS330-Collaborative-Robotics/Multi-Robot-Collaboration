957f741a85f13df86b34c1c6f9d9858c1edb5379
Tom Richards <tomtommrichards@gmail.com>
2022-11-17 13:43:10 +0000

    starting writing the deffintion

---
+++ src/assignment_selection/scripts/selection_prossess.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def selection_prossess():
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
