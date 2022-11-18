2ce9eff9009de2f411196dbb84ed70ea556073a1
Tom Richards <tomtommrichards@gmail.com>
2022-11-18 15:11:44 +0000

    oh thats a funky error, ading Path

--- src/assignment_selection/scripts/arena_locals.py
+++ src/assignment_selection/scripts/arena_locals.py
@@ -2,6 +2,9 @@
 
 import rospy
 from std_msgs.msg import String
+import sys
+sys.path.append("~/catkin_ws/src/assignment_selection/scripts")
+
 from randomBlockLoaclGen import randLocals
 
 def arena_locals():
@@ -9,7 +12,7 @@ def arena_locals():
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        loacation_str = "[[9.19, 4.29], [5.43, 9.35], [1.26, 2.66], [3.92, 0.19], [5.18, 9.83], [8.3, 0.05], [0.82, 0.21], [3.97, 3.77], [7.76, 2.64], [4.32, 3.57], [1.45, 7.75], [0.87, 0.77]]"
+        #loacation_str = "[[9.19, 4.29], [5.43, 9.35], [1.26, 2.66], [3.92, 0.19], [5.18, 9.83], [8.3, 0.05], [0.82, 0.21], [3.97, 3.77], [7.76, 2.64], [4.32, 3.57], [1.45, 7.75], [0.87, 0.77]]"
         loacation_str = randLocals(10,2)
         rospy.loginfo(loacation_str)
         pub.publish(loacation_str)
