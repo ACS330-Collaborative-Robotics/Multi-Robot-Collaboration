3b908fbfe0a694f974c18e9980040b49aa568370
Conor12345 <conornichols9@gmail.com>
2022-11-21 13:37:33 +0000

    Adde multi blok support + randomness

--- src/block_controller/scripts/spawn_blocks.py
+++ src/block_controller/scripts/spawn_blocks.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+# http://docs.ros.org/en/noetic/api/gazebo_msgs/html/index-msg.html
+
 import rospy
 from gazebo_msgs.srv import SpawnModel
 from geometry_msgs.msg import Pose
+from pathlib import Path
 
-import random
+from random import random
 
 def spawner():
     rospy.init_node('block_spawner')
@@ -12,17 +15,25 @@ def spawner():
     rospy.wait_for_service('gazebo/spawn_urdf_model')
     urdf_spawner = rospy.ServiceProxy('gazebo/spawn_urdf_model', SpawnModel)
 
-    f = open('/home/conor/catkin_ws/src/block_controller/urdf/block.urdf')
+    f = open(str(Path.cwd()) + '/src/block_controller/urdf/block.urdf')
     urdf = f.read()
 
     pos = Pose()
-    pos.position.x = 1;
-    pos.position.y = 1;
-    pos.position.z = 1;
-    
-    urdf_spawner("block", urdf, "blocks", pos, "world")
-        
+    for block_num in range(5):
+        # position x y z
+        pos.position.x = 2*random() - 1
+        pos.position.y = 2*random() - 1
+        pos.position.z = 0.50
 
+        # quaternion roation w x y z
+        pos.orientation.w = 1
+        pos.orientation.x = random()
+        pos.orientation.y = random()
+        pos.orientation.z = 0
+        
+        #urdf_spawner(model_name, model_xml, model_namespace, Pose initial_pose, reference_frame)
+        print(urdf_spawner("block" + str(block_num), urdf, "blocks", pos, "world"))
+        
 if __name__ == '__main__':
     try:
         spawner()
