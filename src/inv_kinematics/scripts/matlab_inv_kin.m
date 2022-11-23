4dfa0c79aae66af70db629a163db02908286165e
Conor12345 <conornichols9@gmail.com>
2022-11-23 16:13:22 +0000

    Inverse kinematics initial setup

---
+++ src/inv_kinematics/scripts/matlab_inv_kin.m
@@ -0,0 +1,54 @@
+function matlab_inv_kin
+
+tic
+
+mover6 = importrobot('CPMOVER6.urdf')
+mover6.show;
+
+%% Inverse Kinematics
+
+%randConfig = mover6.randomConfiguration;
+%show(mover6, randConfig);
+
+%tform = getTransform(mover6, randConfig, 'link6', 'base_link');
+x = 0.1;
+y = 0.1;
+z = 0.4;
+
+a = deg2rad(45); % Yaw
+b = deg2rad(-45); % Pitch
+c = deg2rad(60); % Roll
+
+a_tform = [cos(a) -sin(a) 0 0;
+           sin(a) cos(a) 0 0;
+           0 0 1 0;
+           0 0 0 1];
+
+b_tform = [cos(b) 0 sin(b) 0;
+           0 1 0 0;
+           -sin(b) 0 cos(b) 0;
+           0 0 0 1];
+
+c_tform = [1 0 0 0;
+           0 cos(c) -sin(c) 0;
+           0 sin(c) cos(c) 0;
+           0 0 0 1];
+
+abc_tform = a_tform*b_tform*c_tform
+
+xyz_tform = [1 0 0 x; 0 1 0 y; 0 0 1 z; 0 0 0 1]
+
+tform = abc_tform*xyz_tform
+
+ik = inverseKinematics('RigidBodyTree', mover6);
+ik.SolverParameters
+
+weights = [0.25 0.25 0.25 1 1 1];
+
+initialGuess = mover6.homeConfiguration;
+
+[configSoln, solnInfo] = ik('link6', tform, weights, initialGuess)
+
+show(mover6, configSoln)
+
+toc
