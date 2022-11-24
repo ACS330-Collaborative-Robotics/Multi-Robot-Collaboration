b3f0c81c2e2ad2d081e13e03f420c167ea1de641
Conor12345 <conornichols9@gmail.com>
2022-11-24 10:26:08 +0000

    Add comments

--- src/inv_kinematics/scripts/inverse_kinematics.m
+++ src/inv_kinematics/scripts/inverse_kinematics.m
@@ -1,11 +1,26 @@
 function [j1, j2, j3, j4, j5, j6] = inverse_kinematics(x, y, z, a, b, c)
 %INVERSE_KINEMATICS Inverse kinematics of 6DOF Mover6 Manipulator
+%   Function loads CPRMover6.urdf then computes the inverse kinematics using
+%   iterative method.
+%
+%   INPUT:
+%   x - x coordinate relative to robot
+%   y - y coordinate relative to robot
+%   z - z coordinate relative to robot
 %   a - yaw
 %   b - pitch
 %   c - roll
+%
+%   OUTPUT:
+%   Joint positions 1->6 starting from base_link, moving towards link6
+%   j1 j2 j3 j4 j5 j6
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Import URDF and setup RigidBodyTree
 mover6 = importrobot('CPMOVER6.urdf');
 
+% Setup rotation matrices - source: http://msl.cs.uiuc.edu/planning/node102.html
 a_tform = [cos(a) -sin(a) 0 0;
            sin(a) cos(a) 0 0;
            0 0 1 0;
@@ -22,27 +37,34 @@ c_tform = [1 0 0 0;
            0 0 0 1];
 
 abc_tform = a_tform*b_tform*c_tform;
-%abc_tform = eye(4);
 
-%xyz_tform = [1 0 0 x; 0 1 0 y; 0 0 1 z; 0 0 0 1];
+% Add xyz transform to rotation matrix
 abc_tform(1, 4) = x;
 abc_tform(2, 4) = y;
 abc_tform(3, 4) = z;
 
+% Final 4x4 transform from base_link to link6
 tform = abc_tform;
 
+% Initiate ik object
 ik = inverseKinematics('RigidBodyTree', mover6);
 
 % First three are orientation, Second three are xyz
 weights = [0.5 0.5 0.5 1 1 0.2];
 
+% Start with initial guess of robot position
 initialGuess = mover6.homeConfiguration;
 
+% Iteratively improve robot position until it reaches optimal position
 [configSoln, solnInfo] = ik('link6', tform, weights, initialGuess);
+
+% Display ik status - "best available" or "success"
 disp(solnInfo.Status)
 
+% Display plot of robot position
 show(mover6, configSoln)
 
+% Output final joint positions
 [j1, j2, j3, j4, j5, j6] = configSoln.JointPosition;
 
 end
