d032b0658db2f465dada88c94081d418371ff974
Conor12345 <conornichols9@gmail.com>
2022-11-27 16:59:01 +0000

    functionised

--- src/inv_kinematics/scripts/inv_kin_srv.py
+++ src/inv_kinematics/scripts/inv_kin_srv.py
@@ -10,45 +10,64 @@ from math import cos, sin
 import matplotlib.pyplot
 from mpl_toolkits.mplot3d import Axes3D
 
+from gazebo_msgs.msg import ModelState
+
+import time
+
 def service(req):
-    print("Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b)))
-    return (req.a + req.b)
-
-def main():
-    rospy.init_node('inverse_kinematics_server')
-
-    #s = rospy.Service('inverse_kinematics', AddTwoInts, handle_add_two_ints)
-
-    chain = ikpy.chain.Chain.from_urdf_file("/home/conor/catkin_ws/src/mover6_description/urdf/CPRMover6.urdf.xacro", active_links_mask=[False, True, True, True, True, True, True])
-
-    x = 0
-    y = 0.1
-    z = 0.5
-
-    a = 0
-    b = 0
-    c = 0
-
-    a_tform = [[cos(a), -sin(a), 0, 0], [sin(a), cos(a), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
-
-    b_tform = [[cos(b), 0, sin(b), 0], [0, 1, 0, 0], [-sin(b), 0, cos(b), 0], [0, 0, 0, 1]]
-
-    c_tform = [[1, 0, 0, 0], [0, cos(c), -sin(c), 0], [0, sin(c), cos(c), 0], [0, 0, 0, 1]]
-
+    x = req.pose.position.x
+    y = req.pose.position.y
+    z = req.pose.position.z
     target_position = [x, y, z]
-    target_orientation = np.eye(3)
+
+    a = req.pose.orientation.x
+    b = req.pose.orientation.y
+    c = req.pose.orientation.z
+
+    a_tform = np.array([[cos(a), -sin(a), 0], [sin(a), cos(a), 0], [0, 0, 1]])
+    b_tform = np.array([[cos(b), 0, sin(b)], [0, 1, 0], [-sin(b), 0, cos(b)]])
+    c_tform = np.array([[1, 0, 0], [0, cos(c), -sin(c)], [0, sin(c), cos(c)]])
+    ab_tform = np.dot(a_tform, b_tform)
+
+    target_orientation = np.dot(ab_tform, c_tform)
+    print(target_orientation)
 
     joints = chain.inverse_kinematics(target_position, target_orientation, orientation_mode="all")
-    rospy.loginfo(joints)
+    print(joints)
 
     cartesian_position = chain.forward_kinematics(joints)
-    rospy.loginfo(cartesian_position)
+    print(cartesian_position)
 
     ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
 
     chain.plot(joints, ax)
+
+    matplotlib.pyplot.xlim([-0.5, 0.5])
+    matplotlib.pyplot.ylim([-0.5, 0.5])
     
     matplotlib.pyplot.show()
+
+    return joints
+
+def main():
+    rospy.init_node('inverse_kinematics_server')
+
+    #s = rospy.Service('inverse_kinematics', ModelState, service)
+
+    global chain
+    chain = ikpy.chain.Chain.from_urdf_file("/home/conor/catkin_ws/src/mover6_description/urdf/CPRMover6.urdf.xacro", active_links_mask=[False, True, True, True, True, True, True])
+    
+    state = ModelState()
+
+    state.pose.position.x = 0.1
+    state.pose.position.y = 0.1
+    state.pose.position.z = 0.5
+
+    state.pose.orientation.x = 0
+    state.pose.orientation.y = 0
+    state.pose.orientation.z = 0
+
+    service(state)
 
     #rospy.spin()
 
