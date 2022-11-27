25ce3a35600318c90f50a06c2617ab6c76adba96
Conor12345 <conornichols9@gmail.com>
2022-11-27 17:58:06 +0000

    Minor updates

--- src/inv_kinematics/scripts/inv_kin_srv.py
+++ src/inv_kinematics/scripts/inv_kin_srv.py
@@ -6,12 +6,13 @@ import ikpy.chain
 import numpy as np
 
 from math import cos, sin
+from pathlib import Path
 
 from inv_kinematics.srv import InvKin
-
 from std_msgs.msg import Float64
 
 def service(req):
+    # x y z Co-ordinates
     x = req.state.pose.position.x
     y = req.state.pose.position.y
     z = req.state.pose.position.z
@@ -21,20 +22,18 @@ def service(req):
     b = req.state.pose.orientation.y
     c = req.state.pose.orientation.z
 
+    # Rotation - Euler angles to 3x3 rotation matrix
     a_tform = np.array([[cos(a), -sin(a), 0], [sin(a), cos(a), 0], [0, 0, 1]])
     b_tform = np.array([[cos(b), 0, sin(b)], [0, 1, 0], [-sin(b), 0, cos(b)]])
     c_tform = np.array([[1, 0, 0], [0, cos(c), -sin(c)], [0, sin(c), cos(c)]])
     ab_tform = np.dot(a_tform, b_tform)
 
     target_orientation = np.dot(ab_tform, c_tform)
-    #print(target_orientation)
 
+    # Inverse Kinematics
     joints = chain.inverse_kinematics(target_position, target_orientation, orientation_mode="all")
-    #print(joints)
 
-    #cartesian_position = chain.forward_kinematics(joints)
-    #print(cartesian_position)
-
+    # Publish joint positions
     for joint_num in range(len(joints)):
         pub = rospy.Publisher(req.state.model_name + "/joint" + str(joint_num) + "_position_controller/command", Float64, queue_size=10)
         pub.publish(joints[joint_num])
@@ -46,8 +45,9 @@ def main():
 
     s = rospy.Service('inverse_kinematics', InvKin, service)
 
+    # Setup inverse kinematics object as global variable so service can use it
     global chain
-    chain = ikpy.chain.Chain.from_urdf_file("/home/conor/catkin_ws/src/mover6_description/urdf/CPRMover6.urdf.xacro", active_links_mask=[False, True, True, True, True, True, True])
+    chain = ikpy.chain.Chain.from_urdf_file(Path.cwd().as_posix() + "/src/mover6_description/urdf/CPRMover6.urdf.xacro", active_links_mask=[False, True, True, True, True, True, True])
     
     rospy.spin()
 
