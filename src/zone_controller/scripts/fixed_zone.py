cf3f281e703d3281076442b88703d157b9fb3a4b
DhagonHunt <andrei.codin10@gmail.com>
2022-12-02 16:19:01 +0000

    msg file added

--- src/zone_controller/scripts/fixed_zone.py
+++ src/zone_controller/scripts/fixed_zone.py
@@ -15,20 +15,13 @@ def talker():
     corner_b2 = [-0.5, 0.5]
     corner_b3 = [0.5, -0.5]
     corner_b4 = [0.5, 0]
-    corner_h1 = [-0.5, -0.5]
-    corner_h2 = [-0.5, 0.5]
-    corner_h3 = [-1, 0.5]
-    corner_h4 = [-1, -0.5]
     mover6_a_zone = Float64MultiArray()
     mover6_b_zone = Float64MultiArray()
-    human_zone = Float64MultiArray()
     mover6_a_zone.data = [corner_a1, corner_a2, corner_a3, corner_a4]
     mover6_b_zone.data = [corner_b1, corner_b2, corner_b3, corner_b4]
-    human_zone.data = [corner_h1, corner_h2, corner_h3, corner_h4]
-
+   
     pub.publish(mover6_a_zone)
     pub.publish(mover6_b_zone)
-    pub.publish(human_zone)
 
 if __name__ == '__main__':
     try:
