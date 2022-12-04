0081da702731733a0a6cf2a81cc534636dd0707a
Conor <34628734+Conor12345@users.noreply.github.com>
2022-12-04 12:32:43 +0000

    Path Planning Structure (#10)
    * Added packaged
    
    * Added class structure
    
    * Update structure
    
    * Maybe fix ros?
    
    * Make it rosrunable
    
    * Refactor invkin to service helper
    
    * Initial setup of pickup
    
    * Add Path Plan service
    
    * More implementation
    
    * Added debug
    
    * Convert to service (broken)
    
    * Big debug
    
    * added todos
    
    * Add docs
    
    * Minor fix

---
+++ src/path_planning/src/path_planner/Movement.py
@@ -0,0 +1,22 @@
+# Name: Movement Class Definition
+# Author: Conor Nichols (cjnichols1@sheffield.ac.uk)
+
+import rospy
+
+class Movement:
+    def __init__(self, serv_helper):
+        self.serv_helper = serv_helper
+    
+    def move(self, pos):
+        """ Safely move to desired position using IK, checking robot will stay within zone
+
+        INPUT: Pose pos
+        OUTPUT: bool Success - Returns True is movement succesful, False if not possible or failed.
+        """
+
+        self.serv_helper.move(pos)
+        
+        #TODO: Force wait until robot has reached desired position. Temp fix:
+        rospy.sleep(1)
+
+        return True #TODO: Implement zone checks
---
+++ src/path_planning/src/path_planner/PickUp.py
@@ -0,0 +1,31 @@
+# Name: Pick Up Class Definition
+# Author: Conor Nichols (cjnichols1@sheffield.ac.uk)
+
+import rospy
+
+class PickUp:
+    def __init__(self, serv_helper, movement):
+        self.serv_helper = serv_helper
+        self.movement = movement
+    
+    def pick(self, block_name):
+        """ Pick up specified block
+
+        INPUT: block_name
+        OUTPUT: bool Success
+        """
+        pose = self.serv_helper.getBlockPos(block_name)
+        
+        # Move 5cm above block
+        pose.position.z += 0.05
+
+        # Set End Effector orientation
+        pose.orientation.x = 0
+        pose.orientation.y = 3.14
+        pose.orientation.z = 0
+        
+        if self.movement.move(pose):
+            rospy.loginfo("Path Planner - Succesfully positioned above block.")
+
+    def moveGripper(self, state):
+        pass
