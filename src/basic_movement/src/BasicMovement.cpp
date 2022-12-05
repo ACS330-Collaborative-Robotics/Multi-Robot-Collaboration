2177036dc5142e8853fbbec56dc6d8c241631138
Conor12345 <conornichols9@gmail.com>
2022-12-05 16:57:00 +0000

    Add tom stuff

---
+++ src/basic_movement/src/BasicMovement.cpp
@@ -0,0 +1,82 @@
+#include <ros/ros.h>
+#include "std_msgs/String.h"
+#include "control_msgs/JointJog.h"
+
+#include <string>
+#include <sstream>
+/* Create node */
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "simple_movement_example");
+	ros::NodeHandle n;
+	
+	/* Create publisher to attach to JointJog */
+	ros::Publisher chatter_pub = n.advertise<control_msgs::JointJog>("/JointJog",1);
+	
+	ros::Rate loop_rate(10);
+	
+	int counter = 0;
+	
+	
+
+	while(ros::ok()) {
+		ros::Duration(2.0).sleep();
+		ROS_INFO("Counter value %d", counter);
+
+		for (int i=1;i<3;i++){
+			if(counter%2==0) {
+				ROS_INFO("Setting up positive message");
+				control_msgs::JointJog msg_start;
+				std::stringstream ss;
+				string temp = "joint"+i;
+				ss << temp;
+
+				msg_start.joint_names.push_back(ss.str());
+				msg_start.velocities.push_back(0.5);
+				msg_start.duration=5; //Unfortunately duration isn't implemented... 
+				ROS_INFO("Sending message");
+				chatter_pub.publish(msg_start);
+				ros::spinOnce();
+				loop_rate.sleep();
+				
+			}
+			if(counter%2==1) {
+				ROS_INFO("Setting up  negative message");
+				control_msgs::JointJog msg_start;
+				std::stringstream ss;
+				string temp = "joint"+i;
+				ss << temp;
+
+				msg_start.joint_names.push_back(ss.str());
+				msg_start.velocities.push_back(-0.5);
+				msg_start.duration=5; //Unfortunately duration isn't implemented... 
+				ROS_INFO("Sending message");
+				chatter_pub.publish(msg_start);
+				ros::spinOnce();
+				loop_rate.sleep();
+			}			
+		}
+		
+		
+		ros::Duration(5.0).sleep();
+
+		for (int i=0;i<6;i++){
+			if(counter%2==1 || counter%2==0) {
+				ROS_INFO("Setting up stop message");
+				control_msgs::JointJog msg_stop;
+				std::stringstream ss;
+				string temp = "joint"+i;
+				ss << temp;
+
+				msg_stop.joint_names.push_back(ss.str());
+				msg_stop.velocities.push_back(0);
+				msg_stop.duration=5; //Unfortunately duration isn't implemented... 
+				ROS_INFO("Sending message");
+				chatter_pub.publish(msg_stop);
+				ros::spinOnce();
+				loop_rate.sleep();
+			}
+		}
+		counter+=1;
+	}
+	return 0;
+}
---
+++ src/basic_movement/src/GoalMovementMover6.cpp
@@ -0,0 +1,94 @@
+#include <ros/ros.h>
+#include "std_msgs/String.h"
+#include "control_msgs/JointJog.h"
+#include "sensor_msgs/JointState.h"
+
+#include <sstream>
+/* Create node */
+float joint1, joint2, joint3, joint4, joint5, joint6;
+bool know_states;
+
+void jointsCallback(const sensor_msgs::JointState::ConstPtr& msg) {
+	int i=0;
+
+	for(std::vector<double>::const_iterator it = msg->position.begin(); it != msg->position.end(); ++it) {
+		if(i==0) {
+			joint1=*it;
+		}
+		if(i==1) {
+			joint2=*it;
+		}
+		if(i==2) {
+			joint3=*it;
+		}
+		if(i==3) {
+			joint4=*it;
+		}
+		if(i==4) {
+			joint5=*it;
+		}
+		if(i==5) {
+			joint6=*it;
+		}
+		i++;
+	}
+	know_states=true;
+	ROS_INFO("Received State %f\t%f\t%f\t%f\t%f\t%f", joint1, joint2, joint3, joint4, joint5, joint6);
+}
+
+
+int main(int argc, char **argv) {
+	know_states=false;
+	ros::init(argc, argv, "goal_movement_example");
+	ros::NodeHandle n;
+
+	/* Create publisher to attach to JointJog */
+	ros::Publisher chatter_pub = n.advertise<control_msgs::JointJog>("/JointJog",1);
+
+	ros::Subscriber chatter_sub = n.subscribe("/joint_states", 1000, jointsCallback);
+
+	ros::Rate loop_rate(10);
+
+	int counter = 0;
+
+	ros::Duration(2.0).sleep();
+	while(ros::ok()) {
+		if(know_states) {
+			float joint1_demand=0.9;
+
+			if(abs(joint1_demand-joint1)>0.04) {
+
+				ROS_INFO("Setting message");
+				control_msgs::JointJog msg_start;
+				std::stringstream ss;
+				ss << "joint1";
+
+				msg_start.joint_names.push_back(ss.str());
+				msg_start.velocities.push_back(0.25*(joint1_demand-joint1)/abs(joint1_demand-joint1));
+				msg_start.duration=5; //Unfortunately duration isn't implemented...
+
+				ROS_INFO("Sending message");
+				chatter_pub.publish(msg_start);
+			}
+			if(abs(joint1_demand-joint1)<0.04) {
+
+				ROS_INFO("Setting message");
+				control_msgs::JointJog msg_start;
+				std::stringstream ss;
+				ss << "joint1";
+
+				msg_start.joint_names.push_back(ss.str());
+				msg_start.velocities.push_back(0);
+				msg_start.duration=5; //Unfortunately duration isn't implemented...
+
+				ROS_INFO("Sending message");
+				chatter_pub.publish(msg_start);
+			}
+		}
+
+		ros::spinOnce();
+		loop_rate.sleep();
+
+	}
+	return 0;
+}
