a6fd07815c5d973641dd02b9dac3d61a0c91b81e
Conor <34628734+Conor12345@users.noreply.github.com>
2022-12-07 12:14:36 +0000

    Zone Controller V1 (#11)
    * zone_controller creation
    
    * additional files
    
    * zone coords talker
    
    * msg file added
    
    * few changes to publishing, not done
    
    * Arranged message output
    
    * Add custom message format
    
    * Updated message formats
    
    * Fix coord
    
    * Added loop
    
    Co-authored-by: DhagonHunt <andrei.codin10@gmail.com>

---
+++ src/zone_controller/scripts/fixed_zone.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+# Name: Robot Arm Zones Coordinates Publisher
+# Author: Andrei Codin (aacodin1@sheffield.ac.uk)
+
+import rospy
+from zone_controller.msg import Coord, Zone, Zones
+
+def talker():
+    pub = rospy.Publisher('zones', Zones, queue_size=10)
+    rospy.init_node('zone_publisher', anonymous=True)
+
+    # mover6_a co-ordinates
+    corner_a1 = Coord()
+    corner_a1.coordinate = [-0.5, -0.5]
+    corner_a2 = Coord()
+    corner_a2.coordinate = [0.5, 0]
+    corner_a3 = Coord()
+    corner_a3.coordinate = [0, 0.5]
+    corner_a4 = Coord()
+    corner_a4.coordinate = [0.5, 0.5]
+
+    # mover6_b co-ordinates
+    corner_b1 = Coord()
+    corner_b1.coordinate = [0, 0.5]
+    corner_b2 = Coord()
+    corner_b2.coordinate = [-0.5, 0.5]
+    corner_b3 = Coord()
+    corner_b3.coordinate = [0.5, -0.5]
+    corner_b4 = Coord()
+    corner_b4.coordinate = [0.5, 0]
+
+    zone_data = [[corner_a1, corner_a2, corner_a3, corner_a4], [corner_b1, corner_b2, corner_b3, corner_b4]]
+
+    robot_namespaces = ["mover6_a", "mover6_b"]
+    
+    r = rospy.Rate(1)
+        
+    while not(rospy.is_shutdown()):
+        zones = []
+        for robot_num in range(len(robot_namespaces)):
+            zone = Zone()
+            zone.zone_name = robot_namespaces[robot_num] + "_zone"
+
+            zone.zone_coords = zone_data[robot_num]
+
+            zones.append(zone)
+
+        pub.publish(zones)
+
+        r.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
