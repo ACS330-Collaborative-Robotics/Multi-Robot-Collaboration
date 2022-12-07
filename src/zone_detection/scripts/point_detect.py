5a3e7091f9263a737f0c2f8946c3437677b22de6
DhagonHunt <andrei.codin10@gmail.com>
2022-12-07 12:51:37 +0000

    Initial Setup of zone_detection

---
+++ src/zone_detection/scripts/point_detect.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Name: Robot Arm Zone Detection
+# Author: Andrei Codin (aacodin1@sheffield.ac.uk)
+
+import rospy
+from shapely.geometry import Point, Polygon
+
+def talker():
+    
