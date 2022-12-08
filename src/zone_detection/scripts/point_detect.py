3ee8948a7c4855c025a54ab634bb66eaa1842e45
DhagonHunt <andrei.codin10@gmail.com>
2022-12-08 11:24:12 +0000

    Few more commands added

--- src/zone_detection/scripts/point_detect.py
+++ src/zone_detection/scripts/point_detect.py
@@ -7,9 +7,14 @@ import rospy
 from shapely.geometry import Point, Polygon
 
 def talker():
-    mover6_a = Point()
-    mover6_b = Point()
-    coords_a = [(-0.5, -0.5), (0.5, 0), (0, 0.5), (0.5, 0.5)]
-    coords_b = [(0, 0.5), (-0.5, 0.5), (0.5, -0.5), (0.5, 0)]
-    zone_a = Polygon(coords_a)
-    zone_b = Polygon(coords_b)
+    while not rospy.is_shutdown():
+        mover6_a = Point()
+        mover6_b = Point()
+        coords_a = [(-0.5, -0.5), (0.5, 0), (0, 0.5), (0.5, 0.5)]
+        coords_b = [(0, 0.5), (-0.5, 0.5), (0.5, -0.5), (0.5, 0)]
+        zone_a = Polygon(coords_a)
+        zone_b = Polygon(coords_b)
+        mover6_a.within(zone_a)
+        mover6_a.within(zone_b)
+        mover6_b.within(zone_b)
+        mover6_b.within(zone_a)
