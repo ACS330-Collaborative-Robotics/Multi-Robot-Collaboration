daad03b49590901fdb13ed9730ddeea42388ce78
DhagonHunt <andrei.codin10@gmail.com>
2022-12-08 11:11:48 +0000

    Basic code for zone_detection

--- src/zone_detection/scripts/point_detect.py
+++ src/zone_detection/scripts/point_detect.py
@@ -7,4 +7,9 @@ import rospy
 from shapely.geometry import Point, Polygon
 
 def talker():
-    
+    mover6_a = Point()
+    mover6_b = Point()
+    coords_a = [(-0.5, -0.5), (0.5, 0), (0, 0.5), (0.5, 0.5)]
+    coords_b = [(0, 0.5), (-0.5, 0.5), (0.5, -0.5), (0.5, 0)]
+    zone_a = Polygon(coords_a)
+    zone_b = Polygon(coords_b)
