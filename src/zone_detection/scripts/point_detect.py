5aef36d82100e07d4a2e26840c9e6c281eeabfb9
DhagonHunt <andrei.codin10@gmail.com>
2022-12-08 13:38:03 +0000

    Added few commands for end effector detection

--- src/zone_detection/scripts/point_detect.py
+++ src/zone_detection/scripts/point_detect.py
@@ -4,17 +4,23 @@
 # Author: Andrei Codin (aacodin1@sheffield.ac.uk)
 
 import rospy
+import tf
 from shapely.geometry import Point, Polygon
 
-def talker():
-    while not rospy.is_shutdown():
-        mover6_a = Point()
-        mover6_b = Point()
-        coords_a = [(-0.5, -0.5), (0.5, 0), (0, 0.5), (0.5, 0.5)]
-        coords_b = [(0, 0.5), (-0.5, 0.5), (0.5, -0.5), (0.5, 0)]
-        zone_a = Polygon(coords_a)
-        zone_b = Polygon(coords_b)
-        mover6_a.within(zone_a)
-        mover6_a.within(zone_b)
-        mover6_b.within(zone_b)
-        mover6_b.within(zone_a)
+if __name__ == '__main__':
+    rospy.init_node('point_detect')
+    mover6_a_data = tf.TransformListener()
+    mover6_b_data = tf.TransformListener()
+    (transl_a,rot_a) = Point(mover6_a_data.lookupTransform('/kink6', '/base_link', rospy.Time(0)))
+    (transl_b,rot_b) = Point(mover6_b_data.lookupTransform('/link6', '/base_link', rospy.Time(0)))
+    mover6_a = (transl_a[1], transl_a[2])
+    mover6_b = (transl_b[1], transl_b[2])
+    coords_a = [(-0.5, -0.5), (0.5, 0), (0, 0.5), (0.5, 0.5)]
+    coords_b = [(0, 0.5), (-0.5, 0.5), (0.5, -0.5), (0.5, 0)]
+    zone_a = Polygon(coords_a)
+    zone_b = Polygon(coords_b)
+    mover6_a.within(zone_a)
+    mover6_a.within(zone_b)
+    mover6_b.within(zone_b)
+    mover6_b.within(zone_a)
+
