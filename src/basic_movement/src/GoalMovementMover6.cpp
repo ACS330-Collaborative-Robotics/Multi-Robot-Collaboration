9d9a27d017db7d5900406be90bda70ffdc019c5b
Tom Richards <tomtommrichards@gmail.com>
2022-12-13 10:29:33 +0000

    anoucing if moveing or stationary, do be tested

--- src/basic_movement/src/GoalMovementMover6.cpp
+++ src/basic_movement/src/GoalMovementMover6.cpp
@@ -17,7 +17,7 @@
 /* Create node */
 float jointdemand_1, jointdemand_2, jointdemand_3, jointdemand_4, jointdemand_5, jointdemand_6;
 float joint1, joint2, joint3, joint4, joint5, joint6;
-bool know_states, know_demands;
+bool know_states, know_demands, moving_state;
 
 void jointsCallback(const sensor_msgs::JointState::ConstPtr& msg) {
 	int i=0;
@@ -47,45 +47,29 @@ void jointsCallback(const sensor_msgs::JointState::ConstPtr& msg) {
 }
 
 void listenerJointAngles(const basic_movement::Joints::ConstPtr& msg){
-	int i=0;/*
-	for(std::vector<float>::const_iterator it = msg->data.begin(); it != msg->data.end(); ++it) {
-		if(i==0) {
-			jointdemand_1=*it;
-		}
-		if(i==1) {
-			jointdemand_2=*it;
-		}
-		if(i==2) {
-			jointdemand_3=*it;
-		}
-		if(i==3) {
-			jointdemand_4=*it;
-		}
-		if(i==4) {
-			jointdemand_5=*it;
-		}
-		if(i==5) {
-			jointdemand_6=*it;
-		}		
-		i++;
-	}
-	//ROS_INFO("Received Goals %f\t%f\t%f\t%f\t%f\t%f", jointdemand_1, jointdemand_2, jointdemand_3, jointdemand_4, jointdemand_5, jointdemand_6);
-	//know_demands = true;
-	*/
+	jointdemand_1=msg->joints[0];
+	jointdemand_2=msg->joints[1];
+	jointdemand_3=msg->joints[2];
+	jointdemand_4=msg->joints[3];
+	jointdemand_5=msg->joints[4];
+	jointdemand_6=msg->joints[5];
+	ROS_INFO("Received Goals %f\t%f\t%f\t%f\t%f\t%f", jointdemand_1, jointdemand_2, jointdemand_3, jointdemand_4, jointdemand_5, jointdemand_6);
+	know_demands = true;
 }
 
 
 int main(int argc, char **argv) {
 	know_states=false;
 	know_demands=false;
+	moving_state=false;
 	ros::init(argc, argv, "goal_movement_example");
 	ros::NodeHandle n;
 
 	/* Create publisher to attach to JointJog */
 	ros::Publisher chatter_pub = n.advertise<control_msgs::JointJog>("/JointJog",1);
+	ros::Publisher moving = n.advertise<std_msgs::String>("/mover6_a/physical/moving_state",1000);
 
 	ros::Subscriber chatter_sub = n.subscribe("/joint_states", 1000, jointsCallback);
-
 	ros::Subscriber joint_demands = n.subscribe("/mover6_a/physical/joint_angles", 10000, listenerJointAngles);
 	
 
@@ -116,6 +100,7 @@ int main(int argc, char **argv) {
 
 					ROS_INFO("Sending message");
 					chatter_pub.publish(msg_start);
+					moving_state = true;
 				}
 				if(abs(joint_demands[i]-jointpos[i])<0.04) {
 
@@ -131,7 +116,20 @@ int main(int argc, char **argv) {
 					ROS_INFO("Sending message");
 					chatter_pub.publish(msg_start);
 				}
+				std_msgs::String state;
+				std::stringstream system_state;
+				if(moving_state == true){
+					system_state << "Moving";
+				}
+				else {
+					system_state << "Stationery";
+				}
+				ROS_INFO(system_state);
+				state.data = system_state.str();
+				chatter_pub.publish(state);
+				ros::spinOnce();
 			}
+
 		}
 
 		ros::spinOnce();
