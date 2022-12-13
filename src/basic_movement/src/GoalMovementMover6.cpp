50b3dd6989491ea03429f087aade5f31032e4e6b
Tom Richards <tomtommrichards@gmail.com>
2022-12-13 12:56:44 +0000

    adding comments - needs spell chaking, goodluck

--- src/basic_movement/src/GoalMovementMover6.cpp
+++ src/basic_movement/src/GoalMovementMover6.cpp
@@ -10,7 +10,7 @@
 
 // TODO:
 // Imploment listener for where to go
-// 
+// Comment code
 
 
 /* Create node */
@@ -18,6 +18,7 @@ float jointdemand_1, jointdemand_2, jointdemand_3, jointdemand_4, jointdemand_5,
 float joint1, joint2, joint3, joint4, joint5, joint6;
 bool know_states, know_demands, moving_state;
 
+// Reciving Joint angles from robot - assingeing them to veriables - ROSINFO to terminal
 void jointsCallback(const sensor_msgs::JointState::ConstPtr& msg) {
 	int i=0;
 	for(std::vector<double>::const_iterator it = msg->position.begin(); it != msg->position.end(); ++it) {
@@ -45,6 +46,7 @@ void jointsCallback(const sensor_msgs::JointState::ConstPtr& msg) {
 	ROS_INFO("Received State %f\t%f\t%f\t%f\t%f\t%f", joint1, joint2, joint3, joint4, joint5, joint6);
 }
 
+// Reviving Joint Demands from topic - assinging to veriable - ROSINFO to terminal
 void listenerJointAngles(const basic_movement::Joints::ConstPtr& msg){
 	jointdemand_1=msg->joints[0];
 	jointdemand_2=msg->joints[1];
@@ -58,35 +60,45 @@ void listenerJointAngles(const basic_movement::Joints::ConstPtr& msg){
 
 
 int main(int argc, char **argv) {
+	// setting up veriables
 	know_states=false;
 	know_demands=false;
 	moving_state=false;
 	ros::init(argc, argv, "goal_movement_example");
 	ros::NodeHandle n;
 
-	/* Create publisher to attach to JointJog */
+	// Create publishers
 	ros::Publisher chatter_pub = n.advertise<control_msgs::JointJog>("/JointJog",1);
 	ros::Publisher moving = n.advertise<std_msgs::String>("/mover6_a/physical/moving_state",1);
 
+	//Creating Subscribers
 	ros::Subscriber chatter_sub = n.subscribe("/joint_states", 1000, jointsCallback);
 	ros::Subscriber joint_demands = n.subscribe("/mover6_a/physical/joint_angles", 10000, listenerJointAngles);
 	
-
 	ros::Rate loop_rate(10);
 
 	int counter = 0;
 
+	// Joints names
 	const char* joints[6]
         = { "joint1", "joint2", "joint3", "joint4", "joint5", "joint6" };
 
 	ros::Duration(2.0).sleep();
 	while(ros::ok()) {
 		if(know_states && know_demands) {
+			// Setting up lists for joints
 			float joint_demands[6]= {jointdemand_1, jointdemand_2, jointdemand_3, jointdemand_4, jointdemand_5, jointdemand_6};
-			float joint_gains[6] = {0.25, 0.25, 0.25, 0.25, 0.25, 0.1};
 			float jointpos[6] = {joint1, joint2, joint3, joint4, joint5, joint6}; 
+
+			// Accuracy of angles
+			float accuracy = 0.04;
+			
+			// Gains of robots (Speeds) joint 1-6, joint 5 and 6 cant go faster
+			float joint_gains[6] = {0.25, 0.25, 0.25, 0.25, 0.25, 0.1};
 			for (int i=0;i<6;i++){
-				if(abs(joint_demands[i]-jointpos[i])>0.04) {
+
+				// Moving joints
+				if(abs(joint_demands[i]-jointpos[i])>accuracy) {
 
 					ROS_INFO("Setting message Go to set point");
 					control_msgs::JointJog msg_start;
@@ -101,7 +113,9 @@ int main(int argc, char **argv) {
 					chatter_pub.publish(msg_start);
 					moving_state = true;
 				}
-				if(abs(joint_demands[i]-jointpos[i])<0.04) {
+
+				// Stopping joints
+				if(abs(joint_demands[i]-jointpos[i])<accuracy) {
 
 					ROS_INFO("Setting message Stay Still");
 					control_msgs::JointJog msg_start;
@@ -117,6 +131,8 @@ int main(int argc, char **argv) {
 				}
 				
 			}
+
+			// Stating if the arm is still moving to topic
 			std_msgs::String msg;
 			std::stringstream ss;
 			if(moving_state == true){
