30ca36a9cafb07d43c5c08dd8ab030f15592f150
DhagonHunt <andrei.codin10@gmail.com>
2022-12-13 13:25:59 +0000

    tf issues

--- src/zone_detection/scripts/point_detect.py
+++ src/zone_detection/scripts/point_detect.py
@@ -6,44 +6,31 @@
 import rospy
 import tf
 from shapely.geometry import Point, Polygon
-from zone_detection.msg import zone_check
+from zone_controller.msg import Zone
 
-def talker():
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "%s", data.data)
+
+def listener():
     while not rospy.is_shutdown():
-        pub = rospy.Publisher('zones', zone_check, queue_size=10)
-        rospy.init_node('point_detect')
+        rospy.init_node('point_detect', anonymous=True)
+        rospy.Subscriber("chatter", Zone, callback)
         mover6_a_data = tf.TransformListener()
-        mover6_b_data = tf.TransformListener()
-        (transl_a,rot_a) = Point(mover6_a_data.lookupTransform('/link6', '/base_link', rospy.Time(0)))
-        (transl_b,rot_b) = Point(mover6_b_data.lookupTransform('/link6', '/base_link', rospy.Time(0)))
-        mover6_a = (transl_a[1], transl_a[2])
-        mover6_b = (transl_b[1], transl_b[2])
-        coords_a = [(-0.5, -0.5), (0.5, 0), (0, 0.5), (0.5, 0.5)]
-        coords_b = [(0, 0.5), (-0.5, 0.5), (0.5, -0.5), (0.5, 0)]
+        #mover6_b_data = tf.TransformListener()
+        (transl_a,rot_a) = mover6_a_data.lookupTransform('/link6', '/base_link', rospy.Time(0))
+        #(transl_b,rot_b) = mover6_b_data.lookupTransform('/link6', '/base_link', rospy.Time(0))
+        mover6_a = Point(transl_a[1], transl_a[2])
+        #mover6_b = Point(transl_b[1], transl_b[2])
+        coords_a = Zone[1]
+        coords_b = Zone[2]
         zone_a = Polygon(coords_a)
         zone_b = Polygon(coords_b)
         pos_a1 = mover6_a.within(zone_a)
         pos_a2 = mover6_a.within(zone_b)
-        pos_b1 = mover6_b.within(zone_b)
-        pos_b2 = mover6_b.within(zone_a)
+        #pos_b1 = mover6_b.within(zone_b)
+        #pos_b2 = mover6_b.within(zone_a)
+        rospy.spin()
 
-        zone_data = [[pos_a1, pos_a2], [pos_b1, pos_b2]]
 
-        robot_namespaces = ["mover6_a", "mover6_b"]
-
-        r = rospy.Rate(1)
-
-        while not(rospy.is_shutdown()):
-            zones=[]
-            for robot_num in range(len(robot_namespaces)):
-                zone = zone_check()
-                zone.zone_name = robot_namespaces[robot_num] + "_zone"
-
-                zone.zone_coords = zone_data[robot_num]
-
-                zones.append(zone)
-
-            pub.publish(zones)
-
-            r.sleep()
-
+if __name__ == '__main__':
+    listener()
