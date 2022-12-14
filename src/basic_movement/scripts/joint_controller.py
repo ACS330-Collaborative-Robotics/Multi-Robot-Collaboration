79f833d402c9dfe91dc6e86799c9b3a4af49728d
Tom Richards <tomtommrichards@gmail.com>
2022-12-14 16:10:48 +0000

    Setting up joint controller

--- src/basic_movement/scripts/joint_controller.py
+++ src/basic_movement/scripts/joint_controller.py
@@ -13,30 +13,44 @@ from basic_movement.msg import Joints
 # Better comments
 # movement demo, pass string when lauching in lauch file
 
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "Angles Recived: %s", data.joints)
+    joint_angles = list(data.joints)
+
 
 def talker():
     # Shouting where i want the robot to move to then changeing to diffent position after getting there
     # Joint lims in radians [[-2.269,2.269],[-0.873,1.047],[-1.920,1.309],[-2.443,2.443],[-1.221,1.047],[-2.094,2.094]]
 
+    # Engabling the Sim and Phiscal
+    enablePhiscal = True
+    enableSIM = True
+
     # Reciving angles
 
     # Cheak robot runnning
+    rospy.init_node('joint_controller', anonymous=True)
 
-    # demand phisical robot
-
-    # demand sim robot
-    
-    for joint_num in range(len(joints)):
-        pub = rospy.Publisher("/mover6_a/joint" + str(joint_num) + "_position_controller/command", Float64, queue_size=10)
-        pub.publish(joints[joint_num])
-
-    pub = rospy.Publisher('/mover6_a/physical/joint_angles', Joints, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    pubPhisic = rospy.Publisher('/mover6_a/physical/joint_angles', Joints, queue_size=10)
     rate = rospy.Rate(0.1) # 10hz
     while not rospy.is_shutdown():
-        joint_angles = posistions = [round(random.uniform(-2.269,2.269), 3), round(random.uniform(-0.873,1.047), 3), round(random.uniform(-1.920,1.309), 3), round(random.uniform(-2.443,2.443), 3), round(random.uniform(-1.221,1.047), 3), round(random.uniform(-2.094,2.094), 3)]
-        rospy.loginfo(joint_angles)
-        pub.publish(joint_angles)
+        # Cheaking system is enabled
+
+
+        # Getting Demand Angels
+        rospy.Subscriber("/mover6_a/joint_angles", Joints, callback)
+
+        # Telling the Phisical robot tomove
+        if enablePhiscal:
+            rospy.loginfo(joint_angles)
+            pubPhisic.publish(joint_angles)
+        
+        # Telling the Sim to move
+        if enableSIM:
+            for joint_num in range(len(joint_angles)):
+                pubSim = rospy.Publisher("/mover6_a/joint" + str(joint_num) + "_position_controller/command", Float64, queue_size=10)
+                pubSim.publish(joint_angles[joint_num])
+
         rate.sleep()
 
 if __name__ == '__main__':
