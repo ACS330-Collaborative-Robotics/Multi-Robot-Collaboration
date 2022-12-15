02bece6284d05548422d4dd52e741b1c9daa081f
tomrrr <tomtommrichards@gmail.com>
2022-12-15 09:24:52 +0000

    Commmenting out redundt code in cmake

--- src/basic_movement/src/listenerincpp.cpp
+++ src/basic_movement/src/listenerincpp.cpp
@@ -1,14 +1,14 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-//#include "basic_movement/Joints.h"
+#include "basic_movement/Joints.h"
 
 #include <string>
 
-/*
+
 void chatterCallback(const basic_movement::Joints::ConstPtr& msg)
 {
     ROS_INFO("[%f, %f, %f, %f, %f, %f]",msg->joints[0],msg->joints[1],msg->joints[2],msg->joints[3],msg->joints[4],msg->joints[5]);
-}*/
+}
 
 int main(int argc, char **argv)
 {
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  //ros::Subscriber sub = n.subscribe("/mover6_a/physical/joint_angles", 1000, chatterCallback);
+  ros::Subscriber sub = n.subscribe("/mover6_a/physical/joint_angles", 1000, chatterCallback);
 
   ros::spin();
 
