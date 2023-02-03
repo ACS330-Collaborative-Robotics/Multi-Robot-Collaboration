24c467e4e616d04745bb8bcad7761d449cdd90b3
DhagonHunt <andrei.codin10@gmail.com>
2023-02-03 11:24:55 +0000

    broken

--- src/zone_detection/scripts/point_detect.py
+++ src/zone_detection/scripts/point_detect.py
@@ -4,9 +4,9 @@
 # Author: Andrei Codin (aacodin1@sheffield.ac.uk)
 
 import rospy
-import tf
+#import tf
 from shapely.geometry import Point, Polygon
-from zone_controller.msg import Zone
+from zone_controller.msg import Zones
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "%s", data.data)
@@ -14,21 +14,21 @@ def callback(data):
 def listener():
     while not rospy.is_shutdown():
         rospy.init_node('point_detect', anonymous=True)
-        rospy.Subscriber("chatter", Zone, callback)
-        mover6_a_data = tf.TransformListener()
+        rospy.Subscriber("chatter", Zones, callback)
+        #mover6_a_data = tf.TransformListener()
         #mover6_b_data = tf.TransformListener()
-        (transl_a,rot_a) = mover6_a_data.lookupTransform('/link6', '/base_link', rospy.Time(0))
+        #(transl_a,rot_a) = mover6_a_data.lookupTransform('/link6', '/base_link', rospy.Time(0))
         #(transl_b,rot_b) = mover6_b_data.lookupTransform('/link6', '/base_link', rospy.Time(0))
-        mover6_a = Point(transl_a[1], transl_a[2])
-        #mover6_b = Point(transl_b[1], transl_b[2])
-        coords_a = Zone[1]
-        coords_b = Zone[2]
+        mover6_a = Point(0.2, 0.1)
+        mover6_b = Point(-0.2, -0.1)
+        coords_a = Zones(1)
+        coords_b = Zones(2)
         zone_a = Polygon(coords_a)
         zone_b = Polygon(coords_b)
         pos_a1 = mover6_a.within(zone_a)
         pos_a2 = mover6_a.within(zone_b)
-        #pos_b1 = mover6_b.within(zone_b)
-        #pos_b2 = mover6_b.within(zone_a)
+        pos_b1 = mover6_b.within(zone_b)
+        pos_b2 = mover6_b.within(zone_a)
         rospy.spin()
 
 
