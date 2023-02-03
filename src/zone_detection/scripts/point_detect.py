ecae78b7129c6c4dcecfd191a6d1053d72eef30c
DhagonHunt <andrei.codin10@gmail.com>
2023-02-03 12:29:41 +0000

    works

--- src/zone_detection/scripts/point_detect.py
+++ src/zone_detection/scripts/point_detect.py
@@ -11,8 +11,6 @@ from zone_controller.msg import Zones
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "%s", data.zone_data)
 
-    print(data.zone_data[0])
-
     # To be replaced with Forward Kinematics
     mover6_a = Point(0.2, 0.1)
     mover6_b = Point(-0.2, -0.1)
@@ -21,14 +19,22 @@ def callback(data):
     coords_a = data.zone_data[0]
     coords_b = data.zone_data[1] # TODO: Make this work for N zones
 
-    # Zone detection inness
-    zone_a = Polygon(coords_a.zone_coords) # TODO: Convert from object to 2D Array
-    zone_b = Polygon(coords_b.zone_coords)
+    # Zone creation
+    coords_za = [(coords_a.zone_coords[0].coordinate),(coords_a.zone_coords[1].coordinate),(coords_a.zone_coords[2].coordinate),(coords_a.zone_coords[3].coordinate)]
+    coords_zb = [(coords_b.zone_coords[0].coordinate),(coords_b.zone_coords[1].coordinate),(coords_b.zone_coords[2].coordinate),(coords_b.zone_coords[3].coordinate)]
 
+    # Zone detection inness
+    zone_a = Polygon(coords_za) # TODO: Convert from object to 2D Array
+    zone_b = Polygon(coords_zb)
+    print(zone_a)
     pos_a1 = mover6_a.within(zone_a)
     pos_a2 = mover6_a.within(zone_b)
     pos_b1 = mover6_b.within(zone_b)
     pos_b2 = mover6_b.within(zone_a)
+    print(pos_a1)
+    print(pos_a2)
+    print(pos_b1)
+    print(pos_b2)
 
 def listener():
     while not rospy.is_shutdown():
