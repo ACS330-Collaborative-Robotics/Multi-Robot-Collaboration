dfdf7ec0401539c2b81cd217c327e50c74a7fcd0
JFields2501 <JFields1@sheffield.ac.uk>
2023-02-19 17:17:23 +0000

    single launch file and quality of life

--- src/fiducial_recognition/nodes/tf2_listener.py
+++ src/fiducial_recognition/nodes/tf2_listener.py
@@ -1,32 +1,37 @@
 #!/usr/bin/python3
-import rospy
 
+# Name: Block Position Publisher from apriltag_ros transforms
+# Author: Joseph Fields (jfields1@sheffield.ac.uk)
+import rospy
+import sys
 import math
 import tf2_ros
 import geometry_msgs.msg
-from block_controller.msg import block, blocks
+from block_controller.msg import Block, Blocks
+import tf.transformations
 
 
 if __name__ == '__main__':
     rospy.init_node('tf2_listener')
-    print('tf2 Listener Online')
     tfBuffer = tf2_ros.Buffer()
-    listener = tf2_ros.TransformListener(tfBuffer)
+    listener = tf2_ros.TransformListener(tfBuffer) #create transform listener
+    pub = rospy.Publisher('/blocks_pos', Blocks, queue_size=10)
+    rate = rospy.Rate(float(sys.argv[1])) #default polling and publishing rate
 
-    rate = rospy.Rate(10.0)
+    rospy.loginfo("Node started: tf2_listener - polling rate %s Hz",sys.argv[1])
     while not rospy.is_shutdown():
         blocks = []
-        for i in range(2,35):
+        for i in range(2,35): #cycle through tags 2-34
             block = Block()
             tagID='tag_'+str(i)
             try:
-                trans = tfBuffer.lookup_transform('tag_0', tagID, rospy.Time(0))
+                trans = tfBuffer.lookup_transform('tag_0', tagID, rospy.Time(0)) # get transform between tag_0 abd
                 block.block_number=i
-                block.x=trans.transform.translation.x
+                block.x=trans.transform.translation.x #unit: meters
                 block.y=trans.transform.translation.y
                 block.z=trans.transform.translation.z
 
-                (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(float(trans.transform.rotation.x),float(trans.transform.rotation.y),float(trans.transform.rotation.z),float(trans.transform.rotation.w))
+                (roll, pitch, yaw) = tf.transformations.euler_from_quaternion([float(trans.transform.rotation.x),float(trans.transform.rotation.y),float(trans.transform.rotation.z),float(trans.transform.rotation.w)])
                 block.a=pitch
                 block.b=roll
                 block.c=yaw
@@ -34,6 +39,7 @@ if __name__ == '__main__':
                 blocks.append(block)
             except (tf2_ros.ConnectivityException,tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
                 continue  
-         
+        pub.publish(blocks)
+        
         rate.sleep()
 
