845f0258ed809ad27ac4700e2f26b04cc9fdde64
Conor12345 <conornichols9@gmail.com>
2023-03-02 18:40:05 +0000

    More setup of ik service

--- src/inv_kinematics/src/inverse_kinematics_service.cpp
+++ src/inv_kinematics/src/inverse_kinematics_service.cpp
@@ -3,7 +3,23 @@
 
 using namespace std;
 
-int main() {
+bool inverse_kinematics(inv_kinematics::InvKin::Request &req, inv_kinematics::InvKin::Response &res) {
+    cout << req << "\n";
+    cout << res << "\n";
+
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "inverse_kinematics_server");
+    ros::NodeHandle n;
+    ROS_INFO("Inverse Kinematics Service Started.");
+    
+
+    ros::ServiceServer service = n.advertiseService("inverse_kinematics", inverse_kinematics);
+
+    ros::spin();
 
     return 0;
 }
