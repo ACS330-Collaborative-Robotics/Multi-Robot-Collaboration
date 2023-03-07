eec8685d84b37908287b311d1f78171fc6eac052
JFields2501 <JFields1@sheffield.ac.uk>
2023-03-07 10:51:34 +0000

    Add x100 because numbers arent real

--- src/path_planning/src/path_planner/Movement.py
+++ src/path_planning/src/path_planner/Movement.py
@@ -15,9 +15,10 @@ class Movement:
         INPUT: Pose pos
         OUTPUT: bool Success - Returns True is movement succesful, False if not possible or failed.
         """
+        SF=100
         start_pose=self.serv_helper.getJointPos(self.serv_helper.robot_ns,self.serv_helper.robot_ns,6)
-        startx = start_pose.position.x #start position for arm (now relative)
-        starty = start_pose.position.y
+        startx = start_pose.position.x*SF #start position for arm (now relative)
+        starty = start_pose.position.y*SF
 
         pos_robot_frame = self.serv_helper.frameConverter(self.serv_helper.robot_ns, "world", pos) #get desired pose in robot frame
         
@@ -27,20 +28,23 @@ class Movement:
         pos_robot_frame.orientation.z = euler_angles[2]
         pos_robot_frame.orientation.w = 0 #currently no angle stuff
 
-        xgoal = pos_robot_frame.position.x #position of goal x
-        ygoal = pos_robot_frame.position.y
+        xgoal = pos_robot_frame.position.x*SF #position of goal x
+        ygoal = pos_robot_frame.position.y*SF
 
-        xobj = [5] #obstacles will add for loop to look at other arm
-        yobj = [5]
-        Q = 20
+        xobj = [50] #obstacles will add for loop to look at other arm
+        yobj = [50]
+        Q = 15
         D = 10
 
         ##Visual Commands
-        X, Y, xline, yline, PotentialEnergy, EnergyPathTaken, PathTaken = self.serv_helper.Space_Generation(startx, starty, xgoal, ygoal, xobj, yobj, Q, D)
+        X, Y, xline, yline, PotentialEnergy, EnergyPathTaken, PathTakenSF = self.serv_helper.Space_Generation(startx, starty, xgoal, ygoal, xobj, yobj, Q, D)
         self.serv_helper.plotAPF(X, Y, xline, yline, PotentialEnergy, EnergyPathTaken)
-        self.serv_helper.plotPath(PathTaken)
+        self.serv_helper.plotPath(PathTakenSF)
         ##X,Y path the End effector will take
-        PathTaken = self.serv_helper.PathPlanner(startx,starty,xgoal, ygoal, xobj, yobj, Q, D)
+        PathTakenSF = self.serv_helper.PathPlanner(startx,starty,xgoal, ygoal, xobj, yobj, Q, D)
+        
+        PathTaken = [[x[0]/SF,x[1]/SF] for x in PathTakenSF]
+
         print(len(PathTaken))
         ## add a while loop to move through the points?
         tempPos=Pose()
