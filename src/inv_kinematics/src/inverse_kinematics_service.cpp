51a17f4f089aa5206d9193e216328c0b654c3545
Conor12345 <conornichols9@gmail.com>
2023-03-07 12:27:20 +0000

    C++ trac ik initial upload

--- src/inv_kinematics/src/inverse_kinematics_service.cpp
+++ src/inv_kinematics/src/inverse_kinematics_service.cpp
@@ -1,11 +1,32 @@
 #include "ros/ros.h"
 #include "inv_kinematics/InvKin.h"
+#include <trac_ik/trac_ik.hpp>
 
 using namespace std;
 
 bool inverse_kinematics(inv_kinematics::InvKin::Request &req, inv_kinematics::InvKin::Response &res) {
     cout << req << "\n";
     cout << res << "\n";
+    string base_link = "base_link";
+    string tip_link = "link_6";
+
+    TRAC_IK::TRAC_IK ik_solver(base_link, tip_link, string URDF_param="/robot_description", double timeout_in_secs=0.005, double error=1e-5, TRAC_IK::SolveType type=TRAC_IK::Speed);  
+
+    // NOTE: The last arguments to the constructors are optional.
+    // The type can be one of the following: 
+    // Speed: returns very quickly the first solution found
+    // Distance: runs for the full timeout_in_secs, then returns the solution that minimizes SSE from the seed
+    // Manip1: runs for full timeout, returns solution that maximizes sqrt(det(J*J^T)) (the product of the singular values of the Jacobian)
+    // Manip2: runs for full timeout, returns solution that minimizes the ratio of min to max singular values of the Jacobian.
+
+    int rc = ik_solver.CartToJnt(KDL::JntArray joint_seed, KDL::Frame desired_end_effector_pose, KDL::JntArray& return_joints, KDL::Twist tolerances);
+
+    // NOTE: CartToJnt succeeded if rc >=0   
+
+    // NOTE: tolerances on the end effector pose are optional, and if not
+    // provided, then by default are 0.  If given, the ABS() of the
+    // values will be used to set tolerances at -tol..0..+tol for each of
+    // the 6 Cartesian dimensions of the end effector pose.
 
     return true;
 }
