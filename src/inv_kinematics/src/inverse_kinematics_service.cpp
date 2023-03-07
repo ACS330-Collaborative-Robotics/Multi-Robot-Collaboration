81153116d571fd8119859fbae7a055a755432d90
Conor12345 <conornichols9@gmail.com>
2023-03-07 21:54:02 +0000

    C++ inv kin updates

--- src/inv_kinematics/src/inverse_kinematics_service.cpp
+++ src/inv_kinematics/src/inverse_kinematics_service.cpp
@@ -7,11 +7,16 @@ using namespace std;
 bool inverse_kinematics(inv_kinematics::InvKin::Request &req, inv_kinematics::InvKin::Response &res) {
     cout << req << "\n";
     cout << res << "\n";
+
     string base_link = "base_link";
     string tip_link = "link_6";
+    string URDF_param="/robot_description";
+    double timeout_in_secs=0.005;
+    double error=1e-5; 
+    TRAC_IK::SolveType type=TRAC_IK::Speed;
 
-    TRAC_IK::TRAC_IK ik_solver(base_link, tip_link, string URDF_param="/robot_description", double timeout_in_secs=0.005, double error=1e-5, TRAC_IK::SolveType type=TRAC_IK::Speed);  
-
+    TRAC_IK::TRAC_IK ik_solver(string base_link, string tip_link, string URDF_param="/robot_description", double timeout_in_secs=0.005, double error=1e-5, TRAC_IK::SolveType type=TRAC_IK::Speed);
+    
     // NOTE: The last arguments to the constructors are optional.
     // The type can be one of the following: 
     // Speed: returns very quickly the first solution found
@@ -19,7 +24,7 @@ bool inverse_kinematics(inv_kinematics::InvKin::Request &req, inv_kinematics::In
     // Manip1: runs for full timeout, returns solution that maximizes sqrt(det(J*J^T)) (the product of the singular values of the Jacobian)
     // Manip2: runs for full timeout, returns solution that minimizes the ratio of min to max singular values of the Jacobian.
 
-    int rc = ik_solver.CartToJnt(KDL::JntArray joint_seed, KDL::Frame desired_end_effector_pose, KDL::JntArray& return_joints, KDL::Twist tolerances);
+    //int rc = ik_solver.CartToJnt(KDL::JntArray joint_seed, KDL::Frame desired_end_effector_pose, KDL::JntArray& return_joints, KDL::Twist tolerances);
 
     // NOTE: CartToJnt succeeded if rc >=0   
 
