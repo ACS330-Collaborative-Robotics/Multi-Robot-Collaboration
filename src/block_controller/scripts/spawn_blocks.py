9875dbd791be74f65dd3a79aad149d6688520bef
Conor12345 <conornichols9@gmail.com>
2023-03-14 21:59:57 +0000

    Update block spawner

--- src/block_controller/scripts/spawn_blocks.py
+++ src/block_controller/scripts/spawn_blocks.py
@@ -8,7 +8,8 @@ from gazebo_msgs.srv import SpawnModel
 from geometry_msgs.msg import Pose
 from pathlib import Path
 
-from random import random
+from random import random, randint
+from math import pi, cos, sin
 
 def spawner():
     # Setup Node
@@ -22,15 +23,36 @@ def spawner():
     f = open(str(Path.home()) + '/catkin_ws/src/block_controller/urdf/block.urdf')
     urdf = f.read()
 
+    # Spawn blocks in radius around each robot base with a minimum and maximum distance
+    robot_base_coords = [[0,0], [0, 0.5]]
+
+    min_range = 0.15
+    max_range = 0.35
+
     pos = Pose() # Pose object to be filled randomly
     for block_num in range(20):
-        # position x y z
-        x_range = 0.6
-        y_range = 1
+        # Using angle + distance to select random location within range
+        angle = 2*pi*random()
+        distance = (max_range-min_range)*random() + min_range
 
-        pos.position.x = x_range*random() - x_range/2
-        pos.position.y = y_range*random() - y_range/4
+        # Standard 2x2 rotation matrix transformation
+        x = cos(angle) * distance + sin(angle) * 0
+        y = -sin(angle) * distance + cos(angle) * 0
+
+        # Select a robot base randomly
+        robot_num = randint(0, len(robot_base_coords)-1)
+
+        # Assign position, offset by robot base coordinates
+        pos.position.x = x + robot_base_coords[robot_num][0]
+        pos.position.y = y + robot_base_coords[robot_num][1]
         pos.position.z = 0.01
+
+        # Ensure blocks do not spawn "behind" robot becoming unreachable
+        if robot_num == 0 and pos.position.y < -0.2:
+            pos.position.y = -0.2
+        
+        if robot_num == 1 and pos.position.y > 0.7:
+            pos.position.y = 0.7
 
         # quaternion roation w x y z
         pos.orientation.w = 2*random() - 1
