53254c29007663d628bd15c65e7224d8bc293665
Annan2001 <annanthavel2001@yahoo.com>
2023-03-16 09:58:51 +0000

    quaternions added

--- src/assignment_selection/scripts/test_arrays.py
+++ src/assignment_selection/scripts/test_arrays.py
@@ -6,7 +6,7 @@ from block_controller.msg import Blocks
 from path_planning.srv import PathPlan
 from std_msgs.msg import String
 from geometry_msgs.msg import Pose
-
+import tf
 import math
 from operator import itemgetter
 
@@ -14,29 +14,28 @@ n = 20
 layers = math.ceil(n/3)
 print(layers)
 tower_pos = [] #this has to be a 3 column * layers(value) matrix
-h=0.5
-angle=0
+h=0
+rot=0
+
 
 for i in range(layers):
     w=0.25
-    home_pos = [w,0,h,angle]
+    home_pos = [w,0,h,0,0,rot]
     for j in range(3):
-        home_pos = [w,0,h,angle]
+        home_pos = [w,0,h,0,0,rot]
         tower_pos.append(home_pos)
         w=w+1
     h=h+1
 
-    if angle==0:
-        angle=90
-    elif angle==90:
-        angle=0
-    
-    
+    if rot==0:
+        rot=90*(math.pi/180)
+    elif rot==90*(math.pi/180):
+        rot=0
+
 
     
     
 height=len(tower_pos)
 print(tower_pos)
-print(str(height))
 
         
