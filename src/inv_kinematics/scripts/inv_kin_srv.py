45d0957b11fb628c07b44be8b2ea5a3bb7a2e7b5
Conor12345 <conornichols9@gmail.com>
2023-03-23 21:24:12 +0000

    More log info changes

--- src/inv_kinematics/scripts/inv_kin_srv.py
+++ src/inv_kinematics/scripts/inv_kin_srv.py
@@ -74,14 +74,6 @@ def trac_ik_inverse_kinematics(pose: Pose, final_link_name="link6"):
     angle_tolerance = pi/180 # Start with 1 degree tolerance
 
     joints = ik_solver.get_ik(seed_state, pose.position.x, pose.position.y, pose.position.z, pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w, coordinate_tolerance, coordinate_tolerance, coordinate_tolerance, angle_tolerance, angle_tolerance, angle_tolerance)
-
-    #multiplier = 10
-    #while joints is None:
-    #    coordinate_tolerance = coordinate_tolerance * multiplier
-    #
-    #    print("Inverse Kinematics - Trac Ik: Failed to find solution, increasing tolerance by 10 times to", coordinate_tolerance, angle_tolerance)
-    #
-    #    joints = ik_solver.get_ik(seed_state, pose.position.x, pose.position.y, pose.position.z, pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w, coordinate_tolerance, coordinate_tolerance, coordinate_tolerance, angle_tolerance, angle_tolerance, angle_tolerance)
         
     if joints is None:
         return None
