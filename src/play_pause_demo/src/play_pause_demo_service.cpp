a1d27a93dba43eef440e6f18358c1a74daea168f
Conor12345 <conornichols9@gmail.com>
2023-03-24 11:40:05 +0000

    Add service setup

--- src/play_pause_demo/src/play_pause_demo_service.cpp
+++ src/play_pause_demo/src/play_pause_demo_service.cpp
@@ -1,11 +1,21 @@
 #include "ros/ros.h"
 #include "custom_msgs/PlayPause.h"
 
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "play_pause_service");
+bool play_pause(custom_msgs::PlayPause::Request  &req, custom_msgs::PlayPause::Response &res) {
+    std::cout << req;
 
-  ros::NodeHandle n;
+    return true;
+}
 
-  return 0;
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "play_pause_service");
+
+    ros::NodeHandle n;
+
+    ros::ServiceServer service = n.advertiseService("play_pause_demo_service", play_pause);
+    ROS_INFO("Play/Pause service started.");
+    ros::spin();
+
+    return 0;
 }
