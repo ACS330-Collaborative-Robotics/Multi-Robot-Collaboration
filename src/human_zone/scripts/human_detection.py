8152f59ee1e2c6875cf98a4b5be54598740c5fe7
Conor12345 <conornichols9@gmail.com>
2023-03-29 12:27:31 +0100

    Convert to emergency stop channel

--- src/human_zone/scripts/human_detection.py
+++ src/human_zone/scripts/human_detection.py
@@ -7,18 +7,19 @@ def callback(scan):
     # Process the laser scan data to detect humans within 30cm
     # If a human is detected, publish a message to the "human_detection" topic
     human_detected = False
-    for i, r in enumerate(scan.ranges):
+    for r in enumerate(scan.ranges):
         if r < 0.3:
             human_detected = True
             break
+        
     if human_detected:
         rospy.loginfo("Human detected within 30cm!")
-        lidar_pub.publish(True)
+        e_stop_pub.publish(True)
     else:
-        lidar_pub.publish(False)
+        e_stop_pub.publish(False)
 
 if __name__ == '__main__':
     rospy.init_node('human_zone')
-    lidar_pub = rospy.Publisher('human_detection', Bool, queue_size=10)
+    e_stop_pub = rospy.Publisher('emergency_stop', Bool, queue_size=10)
     sub = rospy.Subscriber('/scan', LaserScan, callback)
     rospy.spin()
