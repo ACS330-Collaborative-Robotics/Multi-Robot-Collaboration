5e1390371e478037b464cfe66c541d3cd237c322
Conor12345 <conornichols9@gmail.com>
2023-03-29 13:03:15 +0100

    PickUp tweaks

--- src/path_planning/src/path_planner/PickUp.py
+++ src/path_planning/src/path_planner/PickUp.py
@@ -18,18 +18,23 @@ class PickUp(Movement.Movement):
         OUTPUT: bool Success
         """
         pose = self.serv_helper.getBlockPos(block_name)
-        
+
+        block_orientation_in_quaternion = [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]
+        block_orientation_in_euler = tf_conversions.transformations.euler_from_quaternion(block_orientation_in_quaternion)
+
+        rospy.logwarn("Path Planner - Pick Up - x: %.2f\ty: %.2f\tz: %.2f", block_orientation_in_quaternion[0]*180/pi, block_orientation_in_quaternion[1]*180/pi, block_orientation_in_quaternion[2]*180/pi)
+
         # Move 5cm above block
-        pose.position.z += 0.15
+        pose.position.z += 0.3
 
         # Set End Effector orientation to point downwards using quaternions
-        orientation_in_euler = [0,180*pi/180,0]
-        orientation = tf_conversions.transformations.quaternion_from_euler(orientation_in_euler[0], orientation_in_euler[1], orientation_in_euler[2])
+        orientation_in_euler = [0,180*pi/180, block_orientation_in_euler[2]]
+        orientation_quaternion = tf_conversions.transformations.quaternion_from_euler(orientation_in_euler[0], orientation_in_euler[1], orientation_in_euler[2])
         
-        pose.orientation.x = orientation[0]
-        pose.orientation.y = orientation[1]
-        pose.orientation.z = orientation[2]
-        pose.orientation.w = orientation[3]
+        pose.orientation.x = orientation_quaternion[0]
+        pose.orientation.y = orientation_quaternion[1]
+        pose.orientation.z = orientation_quaternion[2]
+        pose.orientation.w = orientation_quaternion[3]
         rospy.loginfo("Path Planner - Pick Up - Moving to %s", block_name)
         
         return self.move(pose)
