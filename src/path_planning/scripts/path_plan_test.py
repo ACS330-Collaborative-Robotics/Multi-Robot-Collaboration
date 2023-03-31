424739ab5355f591bf39d0b639d586cc20f056b5
Conor12345 <conornichols9@gmail.com>
2023-03-31 12:08:35 +0100

    Add block spawner

--- src/path_planning/scripts/path_plan_test.py
+++ src/path_planning/scripts/path_plan_test.py
@@ -3,8 +3,12 @@
 
 import rospy
 import actionlib
+from pathlib import Path
+import tf
 
 from path_planning.msg import PathPlanAction, PathPlanGoal
+from gazebo_msgs.srv import SpawnModel
+from geometry_msgs.msg import Pose
 
 from math import pi
 
@@ -34,14 +38,14 @@ def path_plan_test():
     final_robot_orientation_z = 0*pi/180
 
     # Block Cartesian Position
-    initial_robot_position_x = 0.1
-    initial_robot_position_y = 0.1
-    initial_robot_position_z = 0.02
+    block_robot_position_x = 0.1
+    block_robot_position_y = 0.1
+    block_robot_position_z = 0.05
 
     # Block Euler Angle Orientation
-    initial_robot_orientation_x = 0*pi/180
-    initial_robot_orientation_y = 0*pi/180
-    initial_robot_orientation_z = 45*pi/180
+    block_robot_orientation_x = 0*pi/180
+    block_robot_orientation_y = 0*pi/180
+    block_robot_orientation_z = 90*pi/180
 
     ######################################
     ##  End of Configurable parameters  ##
@@ -57,14 +61,37 @@ def path_plan_test():
     path_client = actionlib.SimpleActionClient('path_planner', PathPlanAction)
     path_client.wait_for_server()
 
-    #TODO Spawn Block
+    ## Spawn Block ##
+    block_name = "block0"
+
+    # Open Block URDF
+    f = open(str(Path.home()) + '/catkin_ws/src/block_controller/urdf/block.urdf')
+    urdf = f.read()
+
+    # Create Block Pose object
+    block_pose = Pose()
+    block_pose.position.x = block_robot_position_x
+    block_pose.position.y = block_robot_position_y
+    block_pose.position.z = block_robot_position_z
+
+    quat = tf.transformations.quaternion_from_euler(block_robot_orientation_x, block_robot_orientation_y, block_robot_orientation_z)
+    block_pose.orientation.x = quat[0]
+    block_pose.orientation.y = quat[1]
+    block_pose.orientation.z = quat[2]
+    block_pose.orientation.w = quat[3]
+
+    # Setup URDF spawner service
+    rospy.wait_for_service('gazebo/spawn_urdf_model')
+    urdf_spawner = rospy.ServiceProxy('gazebo/spawn_urdf_model', SpawnModel)
+
+    rospy.loginfo(urdf_spawner(block_name, urdf, "blocks", block_pose, "world").status_message)
 
     #TODO Set Initial Pose via IK
 
     #TODO Call Path Planner
 
     #TODO Cleanup Block
-    
+
 
 if __name__ == '__main__':
     try:
