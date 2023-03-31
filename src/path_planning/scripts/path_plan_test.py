954437148970f6eaa9cfd8b6a2145d752cc5177d
Conor12345 <conornichols9@gmail.com>
2023-03-31 12:00:02 +0100

    Add config params

--- src/path_planning/scripts/path_plan_test.py
+++ src/path_planning/scripts/path_plan_test.py
@@ -6,7 +6,47 @@ import actionlib
 
 from path_planning.msg import PathPlanAction, PathPlanGoal
 
+from math import pi
+
 def path_plan_test():
+    ###############################
+    ##  Configurable parameters  ##
+    ###############################
+
+    # Initial Robot Cartesian Position
+    initial_robot_position_x = 0
+    initial_robot_position_y = 0.2
+    initial_robot_position_z = 0.2
+
+    # Initial Robot Euler Angle Orientation
+    initial_robot_orientation_x = 0*pi/180
+    initial_robot_orientation_y = 180*pi/180 # 180 degrees = end effector point down
+    initial_robot_orientation_z = 0*pi/180
+
+    # Final Robot Cartesian Position
+    final_robot_position_x = 0.1
+    final_robot_position_y = 0.3
+    final_robot_position_z = 0.2
+
+    # Final Robot Euler Angle Orientation
+    final_robot_orientation_x = 0*pi/180
+    final_robot_orientation_y = 180*pi/180
+    final_robot_orientation_z = 0*pi/180
+
+    # Block Cartesian Position
+    initial_robot_position_x = 0.1
+    initial_robot_position_y = 0.1
+    initial_robot_position_z = 0.02
+
+    # Block Euler Angle Orientation
+    initial_robot_orientation_x = 0*pi/180
+    initial_robot_orientation_y = 0*pi/180
+    initial_robot_orientation_z = 45*pi/180
+
+    ######################################
+    ##  End of Configurable parameters  ##
+    ######################################
+
     # Declare ROS Node name
     rospy.init_node('path_plan_test')
     
