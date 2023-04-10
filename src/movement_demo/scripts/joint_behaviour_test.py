828281a5423207fd734f45843953466fa96e6d77
Conor12345 <conornichols9@gmail.com>
2023-04-10 17:31:40 +0100

    Setup joint position publisher

--- src/movement_demo/scripts/joint_behaviour_test.py
+++ src/movement_demo/scripts/joint_behaviour_test.py
@@ -26,7 +26,25 @@ def talker():
     initial_angle = initial_angle_degrees * pi/180
     final_angle = final_angle_degrees * pi/180
 
-    joint_position_pub = rospy.Publisher(robot_name + "/joint_angles", Joints, queue_size=10)
+    joint_positions_publisher = rospy.Publisher(robot_name + "/joint_angles", Joints, queue_size=10)
+    rospy.sleep(0.1) # Small delay for publisher to register
+    joint_positions = [0 for i in range(6)]
+
+    ## Initialise listeners
+
+    ## Publish Initial Angle
+    joint_positions[joint_number-1] = initial_angle
+    joint_positions_publisher.publish(joint_positions)
+
+    rospy.sleep(time_delay_seconds)
+
+    ## Publish Final Angle
+    joint_positions[joint_number-1] = final_angle
+    joint_positions_publisher.publish(joint_positions)
+
+    rospy.sleep(time_delay_seconds)
+
+    ## Plot response
 
 if __name__ == '__main__':
     try:
