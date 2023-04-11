0c0a38bdf668fc8c125f40e1de894cd82dd4c118
Conor12345 <conornichols9@gmail.com>
2023-04-11 15:12:21 +0100

    Update test kinematic chain

--- src/movement_demo/scripts/test_kinematic_chain.py
+++ src/movement_demo/scripts/test_kinematic_chain.py
@@ -1,41 +1,60 @@
 import rospy
+import actionlib
+import tf
 
-from path_planning.srv import PathPlan
+from path_planning.msg import PathPlanAction, PathPlanGoal
 from geometry_msgs.msg import Pose
 
 def choose_block():
-    # Setup path_planner service
-    rospy.wait_for_service('path_planner')
-    path_service = rospy.ServiceProxy('path_planner', PathPlan)
+    goal = PathPlanGoal()
+
+    #############################
+    ## Configurable Parameters ##
+    #############################
+
+    goal.robot_name = "mover6_a"
+    goal.block_name = "block18"
+
+    cartesian_coordinates = [0.2, 0, 0.2]
+    orientation_in_euler = [0, 0, 0]
+
+    #############################
 
     # Declare ROS Node name
     rospy.init_node('test_kinematic_chain')
 
-    robot_name = "mover6_a"
-    
-    block_name = "block18"
+    # Setup path_planner action client
+    path_client = actionlib.SimpleActionClient('path_planner', PathPlanAction)
+    path_client.wait_for_server()
 
     end_pos = Pose()
     
-    end_pos.position.x = 0.2
-    end_pos.position.y = 0
-    end_pos.position.z = 0.2
-    
-    end_pos.orientation.x = 0
-    end_pos.orientation.y = 0.707
-    end_pos.orientation.z = 0
-    end_pos.orientation.w = 0.707
+    end_pos.position.x = cartesian_coordinates[0]
+    end_pos.position.y = cartesian_coordinates[1]
+    end_pos.position.z = cartesian_coordinates[2]
+
+    quat = tf.transformations.quaternion_from_euler(orientation_in_euler[0],orientation_in_euler[1],orientation_in_euler[2])
+
+    end_pos.orientation.x = quat[0]
+    end_pos.orientation.y = quat[1]
+    end_pos.orientation.z = quat[2]
+    end_pos.orientation.w = quat[3]
+
+    goal.end_pos = end_pos
     
     rospy.loginfo("Test Kinematic Chain - Calling path planner")
     
-    try:
-        success = path_service(block_name, end_pos, robot_name)
+    path_client.send_goal(goal)
 
-        if not(success):
-            rospy.loginfo("Block Selection - Service call returned False.")
-            
-    except rospy.ServiceException as e:
-        rospy.loginfo("Block Selection - Service call failed: %s"%e)
+    while (path_client.get_state() == 1) and not rospy.is_shutdown():
+        rospy.loginfo_once("Assignment Selection - Waiting for robot %s to complete action.", goal.robot_name)
+        rospy.sleep(0.01)
+
+    status = path_client.get_result().success
+    if status:
+        rospy.loginfo("Assignment Selection - Robot %s action completed successfully.\n", goal.robot_name)
+    else:
+        rospy.logerr("Assignment Selection - Robot %s action failed with status %i.\n", goal.robot_name, status)
 
 if __name__ == '__main__':
     try:
