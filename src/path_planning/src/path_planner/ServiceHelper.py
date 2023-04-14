0454b467751fea85ad0624120e902ba3778640f1
collin147 <crais1471@gmail.com>
2023-04-14 16:35:36 +0100

    added diffreptemp

--- src/path_planning/src/path_planner/ServiceHelper.py
+++ src/path_planning/src/path_planner/ServiceHelper.py
@@ -370,9 +370,9 @@ class ServiceHelper:
             diffreptemp = self.PotentialRepulsionChange(PathPointsx[i],PathPointsy[i],PathPointsz[i],tempxobj,tempyobj,tempzobj,xgoal,ygoal,zgoal,tempQ)
             diffatt = self.PotentialAttractionChange(PathPointsx[i],PathPointsy[i],PathPointsz[i],xgoal,ygoal,zgoal,D)
             if any(diffrep) != 0:
-                difx = diffrep[0] + 0.25*diffatt[0]
-                dify = diffrep[1] + 0.25*diffatt[1]
-                difz = diffrep[2] + 0.25*diffatt[2]
+                difx = diffrep[0] + diffreptemp[0] + 0.25*diffatt[0]
+                dify = diffrep[1] + diffreptemp[1] + 0.25*diffatt[1]
+                difz = diffrep[2] + diffreptemp[2] + 0.25*diffatt[2]
                 rospy.loginfo("Potential Fields - Repulsion strength: %.2f,%.2f,%.2f dist: %.2f",-difx,-dify,-difz,d)
             else:
                 difx = diffatt[0]
@@ -406,7 +406,7 @@ class ServiceHelper:
                     PathPointsx.append(x)
                     PathPointsy.append(y)
                     PathPointsz.append(z)
-            i += 1
+                i += 1
             #rospy.loginfo(PathPointsx[i],PathPointsy[i])
         #PathPoints = list(zip(PathPointsx,PathPointsy))
         return PathPointsx,PathPointsy,PathPointsz
