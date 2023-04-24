241add5281fa2dcb22f2a96279a91a0dd16061df
JFields2501 <JFields1@sheffield.ac.uk>
2023-04-24 16:25:43 +0100

    iterative block hunter

--- src/block_controller/scripts/update_blocks_phys.py
+++ src/block_controller/scripts/update_blocks_phys.py
@@ -15,38 +15,50 @@ import tf_conversions
 blockData_cam = None
 #to call, send True or False 
 def update(data):
+    remainingTags=[10,11,12,13,14,15,16,17,18,19]
+    count=0
+
     # Setup set model state service
     rospy.wait_for_service('gazebo/set_model_state')
     block_updater = rospy.ServiceProxy('gazebo/set_model_state', SetModelState)
 
     #wait for data from camera (MIGHT NOT BE NEEDED)
-    while (blockData_cam is None) and not(rospy.is_shutdown()):
+    while (blockData_cam is None) and not rospy.is_shutdown():
         rospy.loginfo("Block Updater - Waiting for block and camera data.")
         rospy.sleep(0.1)
+
     rospy.loginfo("Block Updater - Got block and camera data.")
+    
+    while len(remainingTags) > 0 and not rospy.is_shutdown():
+        for i in range(len(blockData_cam.block_data)):
+            pos = Pose()
+            model = ModelState()
+            model.model_name = "block" + str(blockData_cam.block_data[i].block_number)
 
-    for i in range(len(blockData_cam.block_data)):
-        pos=Pose()
-        model=ModelState()
-        model.model_name="block"+ str(blockData_cam.block_data[i].block_number)
+            pos.position.x = blockData_cam.block_data[i].x
+            pos.position.y = blockData_cam.block_data[i].y
+            pos.position.z = blockData_cam.block_data[i].z
+            a = blockData_cam.block_data[i].a
+            b = blockData_cam.block_data[i].b
+            c = blockData_cam.block_data[i].c
 
-        pos.position.x=blockData_cam.block_data[i].x
-        pos.position.y=blockData_cam.block_data[i].y
-        pos.position.z=blockData_cam.block_data[i].z
-        a=blockData_cam.block_data[i].a
-        b=blockData_cam.block_data[i].b
-        c=blockData_cam.block_data[i].c
+            orientation = tf_conversions.transformations.quaternion_from_euler(a,b,c)
+            pos.orientation.x = orientation[0]
+            pos.orientation.y = orientation[1]
+            pos.orientation.z = orientation[2]
+            pos.orientation.w = orientation[3]
 
-        orientation = tf_conversions.transformations.quaternion_from_euler(a,b,c)
-        pos.orientation.x = orientation[0]
-        pos.orientation.y = orientation[1]
-        pos.orientation.z = orientation[2]
-        pos.orientation.w = orientation[3]
+            model.pose = pos
+            #model.reference_frame = "world" #might cause issues
+            block_updater(model)
+            remainingTags.remove(blockData_cam.block_data[i].block_number)
+            count = count+1
 
-        model.pose=pos
-        #model.reference_frame="world" #might cause issues
+        if(count < 25):
+            rospy.loginfo("Block Updater - Tags not found: %s",remainingTags)
+        else:
+            rospy.logwarn("Block Updater - Tags STILL not found: %s. Are tags in frame?",remainingTags)
 
-        block_updater(model)
     return True
             
 def callback(data):
