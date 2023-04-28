e2857a11c16cca59fb5009b9a98f122a22240d4b
Conor12345 <conornichols9@gmail.com>
2023-04-28 13:48:00 +0100

    Finish block selection reachability

--- src/assignment_selection/scripts/block_selection.py
+++ src/assignment_selection/scripts/block_selection.py
@@ -297,7 +297,7 @@ def is_block_position_reachable(x, y, z, euler_x, euler_y, euler_z, robot_name):
 
     block_orientation_euler = [euler_x, euler_y, euler_z]
 
-    for angle_offset in [0, -180*math.pi/180, 180*math.pi/180]:
+    for angle_offset in [0, -math.pi, math.pi]:
         orientation_euler = [0, math.pi, block_orientation_euler[2]+angle_offset]
         orientation_quaternion = tf_conversions.transformations.quaternion_from_euler(orientation_euler[0], orientation_euler[1], orientation_euler[2])
         
@@ -314,10 +314,8 @@ def is_block_position_reachable(x, y, z, euler_x, euler_y, euler_z, robot_name):
 
             model_state.pose.position.z += 0.05
             if inv_kin_is_reachable(model_state).success:
-                rospy.logwarn("Assignment Selection - Adding %s as it is reachable by %s", block_name, robot_name)
                 return True
     
-    rospy.loginfo("Assignment Selection - %s cannot reach %s", robot_name, block_name)
     return False
     
 
