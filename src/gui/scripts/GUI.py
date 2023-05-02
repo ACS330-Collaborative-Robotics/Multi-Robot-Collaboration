d20a518bd1f48f179903384066f291878bf038de
wikssss <wszkolnicka1@sheffield.ac.uk>
2023-05-02 20:06:06 +0100

    physical camera feed subscriber update

--- src/gui/scripts/GUI.py
+++ src/gui/scripts/GUI.py
@@ -106,7 +106,7 @@ class GUI:
         self.bridge = CvBridge() 
         rospy.Subscriber('/camera1/image_raw', ImageMsg, self.callback_video)
         # physical camera feed listener
-        rospy.Subscriber()
+        rospy.Subscriber('/tag_detections_image', ImageMsg, self.camera_callback)
 
         # ROS spin loop
         self.thread = threading.Thread(target=rospy.spin) 
