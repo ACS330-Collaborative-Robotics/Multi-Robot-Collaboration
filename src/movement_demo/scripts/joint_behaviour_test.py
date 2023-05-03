375067231dd3336839166d31b6d07fc2881b4791
tomrrr <tomtommrichards@gmail.com>
2023-05-03 11:10:15 +0100

    changinve vales

--- src/movement_demo/scripts/joint_behaviour_test.py
+++ src/movement_demo/scripts/joint_behaviour_test.py
@@ -46,11 +46,11 @@ def talker():
     #############################
 
     robot_name = "mover6_a"
-    joint_number = 6 # Range: 1-6
+    joint_number = 1 # Range: 1-6
 
     # Initial Angle -> Time Delay -> Final Angle -> Time Delay
-    initial_angle_degrees = 45
-    final_angle_degrees = -45
+    initial_angle_degrees = 0
+    final_angle_degrees = 0
     
     time_delay_seconds = 16
 
